cd16395b7e77bb25ce517cfa427de0dcf5c17815
jhon.gonzalez28 <jhon.gonzalez28@88bac944-d0fc-eefa-ac56-35fbec5b5244>
2011-02-12 07:46:49 +0000

    Image Reader

--- VisorImagen/src/Visor/ImageReader.java
+++ VisorImagen/src/Visor/ImageReader.java
@@ -15,7 +15,7 @@ import java.io.IOException;
  *
  * @author Jhon
  */
-public class ImageReader {
+public class ImageReader{
 
     public ImageReader(){
     }
@@ -24,18 +24,112 @@ public class ImageReader {
         byte[] contenidoDelFichero = null;
         FileInputStream theFIS = null;
         BufferedInputStream theBIS = null;
-        byte[] buffer = new byte[2 * 1024];
+        byte[] buffer;
         int leido = 0;
         ByteArrayOutputStream theBOS = new ByteArrayOutputStream();
+        String extencion="";
+        int i=0;
+        ImageData image = new ImageData();
+        for(char a : f.getPath().toCharArray()){
+            if(a=='.' || i > 0){
+                i++;
+                if(i>1){
+                    extencion+=String.valueOf(a);
+                }
+
+            }
+        }
+        image.setFormat(extencion);
+        if(extencion.equals("BMP") || extencion.equals("bmp")){
+            image.setFormato(false);
+            buffer = new byte[54 * 1024];
+            try{
+                theFIS = new FileInputStream(f);
+                theBIS = new BufferedInputStream(theFIS);
+                while ((leido = theBIS.read(buffer)) >= 0){
+                theBOS.write(buffer, 0, leido);
+                }
+                contenidoDelFichero = theBOS.toByteArray();
+                image.setCabecera(contenidoDelFichero);
+
+
+
+
+
+                theBOS.reset();
+                theBOS.close();
+            }
+            catch (IOException e1){
+                e1.printStackTrace();
+            }
+            finally{
+                if (theBIS != null){
+                    try{
+                      theBIS.close();
+                    }
+                    catch (IOException e){
+                    e.printStackTrace();
+                    }
+              }
+            }
+            
+        }else if(extencion.equals("jpg") || extencion.equals("JPG") || extencion.equals("jpeg") || extencion.equals("JPEG")){
+            image.setFormato(false);
+            buffer = new byte[10 * 1024];
+            try{
+                theFIS = new FileInputStream(f);
+                theBIS = new BufferedInputStream(theFIS);
+                while ((leido = theBIS.read(buffer)) >= 0){
+                theBOS.write(buffer, 0, leido);
+                }
+                contenidoDelFichero = theBOS.toByteArray();
+                image.setCabecera(contenidoDelFichero);
+
+
+
+
+
+                theBOS.reset();
+                theBOS.close();
+            }
+            catch (IOException e1){
+                e1.printStackTrace();
+            }
+            finally{
+                if (theBIS != null){
+                    try{
+                      theBIS.close();
+                    }
+                    catch (IOException e){
+                    e.printStackTrace();
+                    }
+                }
+            }
+        }else{
+            
+        }
+
+
+       
+        return image;
+    }
+}
+
+
+
+ /*
 
     try
     {
       theFIS = new FileInputStream(f);
       theBIS = new BufferedInputStream(theFIS);
+      theBIS.read();
       while ((leido = theBIS.read(buffer)) >= 0)
       {
         theBOS.write(buffer, 0, leido);
       }
+
+
       // Fichero leido del todo, pasamos el contenido
       // del BOS al byte[]
       contenidoDelFichero = theBOS.toByteArray();
@@ -66,10 +160,6 @@ public class ImageReader {
         }
       }
     }
-
-
-        ImageData i = new ImageData(2 * 1024,true);
+        ImageData i = new ImageData(8 * 1024,true);
         i.setBits(contenidoDelFichero);
-        return i;
-    }
-}
+        return i;*/

d751150b7b6025361d06cd26d6aff9d3de46623c
luisfmontoya <luisfmontoya@88bac944-d0fc-eefa-ac56-35fbec5b5244>
2011-03-16 06:21:26 +0000

    cTest_LZW funcionando con métodos de encode y decode

--- VisorImagen/src/test_Draw/cTest_LZW.java
+++ VisorImagen/src/test_Draw/cTest_LZW.java
@@ -19,70 +19,185 @@ import java.util.logging.Logger;
 public class cTest_LZW {
 
     byte[] inByteArray;
-    byte[] table;
+    int[] table;
 
-    public cTest_LZW(byte[] byteArray) {
-        this.inByteArray = byteArray;
-
-    }
-
-    public static void main(String args[]){
+    public cTest_LZW() {
         try {
+            
+            // generar un inputSteam
+            String array = "the/rain/in/Spain/falls/mainly/on/the/plain";
+            ByteArrayInputStream bais = new ByteArrayInputStream(array.getBytes());
+            BitInputStream inputStream = new BitInputStream(bais);
 
-            System.out.println("empezando");
-            writeBitStream();
-            readBitStream();
-            System.out.println("terminado");
+//            Ejemplo para generar un bitInputStream desde un array
+//            byte[] array = new byte[100];
+//            ByteArrayInputStream bais = new ByteArrayInputStream(array);
+//            BitInputStream is = new BitInputStream(bais);
 
+            // codificarlo
+            BitOutputStream oStream = encode(inputStream, array.length());
+
+            // decodificarlo
+//            String array2 = "@hP/ aѮ";
+//            ByteArrayInputStream bais2 = new ByteArrayInputStream(array2.getBytes());
+//            BitInputStream iStream = new BitInputStream(bais2);
+            BitInputStream iStream = new BitInputStream("bitOutputStream.txt");
+            decode(iStream);
+
+
+
+//            readBitStream();
 
         } catch (IOException ex) {
             Logger.getLogger(cTest_LZW.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-
-    }
-    public void populateTable(){
-        table = new byte[65536];
-        java.util.Arrays.fill(table, (byte)0x0000);
     }
 
-    public static void writeBitStream() throws IOException{
-
-        BitOutputStream os = new BitOutputStream("bitOutputStream.txt");
-
-        int code = 99999;
-
-        os.writeBits(8,code) ;
-
-        os.flush();
-
-//        BitInputStream is = new BitInputStream("bitOutputStream.txt");
-//
-//        int returnedCode = is.readBits(8);
-//
-//        System.out.println("Leido: " + returnedCode);
+    public static void main(String args[]){
+        new cTest_LZW();
     }
 
-    public static void readBitStream() throws IOException{
-        
-        byte[] array = new byte[200];
-        java.util.Arrays.fill(array, (byte)0xF0F0);
+ 
+    public BitOutputStream encode(BitInputStream inputStream, int length)throws IOException{
+        // Primero se crea un diccionario y se llenan los primeros 256 valores
+        String[] dict = new String[65536];
+        int iTable;
+        for (iTable = 0; iTable<256;iTable++)
+            dict[iTable]=Character.toString((char)iTable);
 
-        array[0]=(byte)0xF0;
-        array[1]=(byte)0x1F;
+
+        // Se crea una variable de salida y otras variables temporales del algoritmo
+        // Los nombres son consistentes con la explicación de http://www.dspguide.com/ch27/5.htm
+        char k;
+        String w = "";
+        String wk;
+        int[] output = new int[65536];
+        int iOutput = 0;
+
+        //Por compatibilidad con el método de descompresión se crea un BitOutputStream
+        BitOutputStream outputStream = new BitOutputStream("bitOutputStream.txt");
+
+//        algoritmo de compresión
+        for (int i = 0; i <= length; i++){
+
+            // primero se obtiene el valor actual y a su vez se concatena con el
+            // último valor de w que el algoritmo tenga
+            k = (char)inputStream.readBits(8);
+            wk = w.concat(Character.toString(k));
 
 
-        ByteArrayInputStream bais = new ByteArrayInputStream(array);
+            // Busca a wk en el diccionario, si lo encuentra retorna verdadero en isOnDict
+            boolean isOnDict = false;
+            for (int j = 0; j < iTable; j++){
+                if(dict[j].equals(wk)){
+                    isOnDict = true;
+                    break;
+                }
+            }
 
-        BitInputStream is2 = new BitInputStream(bais);
 
-        int returnedCode2 = is2.readBits(12);
+            // De acuerdo a la presencia de wk en el diccionario: si esta, lo asigna a w y termina
+            // si no, lo añade al diccionario, y retorna el código de w
+            if (isOnDict){
+               w = wk;
+            }
+            else
+            {
+                // añadir wk
+                dict[iTable++] = wk;
 
-        System.out.println("Leido byteArray: " + returnedCode2);
-         returnedCode2 = is2.readBits(4);
+                //buscar el código de w
+                int codeW = 0;
+                for (int j = 0; j < iTable; j++){
+                    if(dict[j].equals(w)){
+                        codeW = j;
+                        break;
+                    }
+                }
 
-        System.out.println("Leido byteArray: " + returnedCode2);
+                // retornar el código de w como un array de enteros
+                output[iOutput] = codeW;
+
+                int writeBits;
+                if (iOutput < 4096)
+                    writeBits = 12;
+                else
+                    if (iOutput < 8192)
+                        writeBits = 13;
+                    else
+                        if (iOutput < 16384)
+                            writeBits = 14;
+                        else
+                            if (iOutput < 32768)
+                                writeBits = 15;
+                            else
+                                if (iOutput < 65536)
+                                    writeBits = 16;
+                                else
+                                    throw new IOException("La tabla de codificación superó su máximo tamaño");
+
+                outputStream.writeBits(writeBits, codeW);
+                iOutput++;
+
+                
+//                // Opcional: Imprimir en pantalla
+                if (codeW < 256)
+                    System.out.println("output " + (char)codeW);
+                else
+                    System.out.println("output " + codeW);
+
+                // Asigna el valor actual a w para continuar el siguiente ciclo
+                w = Character.toString(k);
+            }
+      }
+        outputStream.flush();
+        return outputStream;
     }
+
+    public void decode(BitInputStream inputStream)throws IOException{
+
+//        inputStream = new BitInputStream("bitOutputStream.txt");
+
+        // Primero se crea un diccionario y se llenan los primeros 256 valores
+        String[] dict = new String[65536];
+        int iTable;
+        for (iTable = 0; iTable<256;iTable++)
+            dict[iTable]=Character.toString((char)iTable);
+
+
+        // Se crea una variable de salida y otras variables temporales del algoritmo
+        // Los nombres son consistentes con la explicación de http://www.dspguide.com/ch27/5.htm
+        int intK;
+        String w = "";
+
+
+        // algoritmo de compresión
+        // Primer valor
+        intK = inputStream.readBits(12);
+        System.out.println("leido " + dict[intK]);  // salida
+        w=dict[intK];
+      
+        while(true){
+
+            intK = inputStream.readBits(12);
+            if (intK == -1){
+                System.out.println("Lectura terminada");
+                break;
+            }
+ 
+            System.out.println("leido " + dict[intK]); // salida
+            w = w.concat(dict[intK]);
+            dict[iTable++] = w;
+
+            w = dict[intK];
+
+        }
+
+
+    }
+
+   
 
 
 

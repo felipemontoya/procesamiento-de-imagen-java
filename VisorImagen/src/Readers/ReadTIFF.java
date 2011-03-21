edd73d5bc5a3e0adf53c050028631a58b5279afe
jhon.gonzalez28@gmail.com <jhon.gonzalez28@gmail.com@88bac944-d0fc-eefa-ac56-35fbec5b5244>
2011-03-21 02:27:58 +0000

    lee pero lee mal :S

--- VisorImagen/src/Readers/ReadTIFF.java
+++ VisorImagen/src/Readers/ReadTIFF.java
@@ -235,19 +235,50 @@ public class ReadTIFF {
             j += 3 * width;
         }
         }else if(compresion==5){
-            
-            
-            
-            
-        ByteArrayInputStream bais = new ByteArrayInputStream(this.CutBytesM(bytesFile, 8, offset));    
-        BitInputStream inputStream = new BitInputStream(bais);   
-        // Primero se crea un diccionario y se llenan los primeros 256 valores    
-         String[] dict = new String[65536];
+
+
+         /*
+          * YA solucione el problema de que se toteara... ahora el problema es q esta leyendo mal...
+          * yo creo q el problema esta en como almacenamos en el diccionario...
+          * utilice 3 metodos q estan todos ahi metidos pero en este comento el METODO 1 es el q esta funcionando
+          * los otros Metodos estan comentados con eso si los quieren ver funcionando toca q comenten el metodo 1
+          * y descomenten todo lo q tenga q ver con el metodo q quieran ver...
+          *
+          *
+          * el metodo 1 es el q esta en Test__LZW
+          * el metodo 2 es con listas y es el implementado en adobe
+          * el metodo 3 esta en esta pagina... el ultimo http://marknelson.us/1989/10/01/lzw-data-compression/ el de modified
+          *
+          * no he dado con el tiro... el 1 y 3 botan bien cuando es blanco... pero el 1 empieza a botar
+          * cadenas muy largas cuando es negro
+          *
+          * en definitiva creo q estamos manejando mal la estructura del diccionario como almacenamos...
+          *
+          * voy a darle a lo de comparacion de imagenes
+          *
+          *
+          */
+
+        ByteArrayInputStream bais = new ByteArrayInputStream(this.CutBytesM(bytesFile, 8, offset));
+        BitInputStream inputStream = new BitInputStream(bais);
+        // Primero se crea un diccionario y se llenan los primeros 256 valores
+        //METODO 1 y 3
+        String[] dict = new String[65536];
         int iTable;
         for (iTable = 0; iTable<256;iTable++)
-            dict[iTable]=Character.toString((char)iTable);
-        
-        
+             dict[iTable]=(Character.toString((char)iTable));
+        dict[iTable++]=(Character.toString((char)-1));
+        dict[iTable++]=(Character.toString((char)-1));
+
+
+        //METODO 2
+        /*List<String> dict = new ArrayList<String>();
+        for (iTable = 0; iTable<256;iTable++)
+            dict.add(Character.toString((char)iTable));
+        dict.add(Character.toString((char)-1));
+        dict.add(Character.toString((char)-1));*/
+
+
         // Se crea una variable de salida y otras variables temporales del algoritmo
         // Los nombres son consistentes con la explicación de http://www.dspguide.com/ch27/5.htm
         //code es el codigo que lee... el k
@@ -257,67 +288,110 @@ public class ReadTIFF {
         // aca dice cuando bit a leido... para hacer que el while finalice
         int bitsread=9;
         //oldcode es el codigo viejo... el w
-        String oldcode = "";
+
+
+
+
+        //METODO 1
+        String oldcode;
         //ese outstring lo vi para concatenar...
-        String outString;
-        /*como siempre el byte 8 (donde inicia los datos de la imagen de un tiff) es el 80 en hexa es decir en decimal 10000000
-         * si empiezo desde ese byte a contar los 9 bits... me botaria 100000000 q es 256 y ese no existe
-         * todabia en el diccionario... entonces lo q hago es leer los primeros 9bits... lo dejo asi... y sigo 
-         */
-        
-        inputStream.readBits(read);
-        //sigo con los siguientes 9 bits
+
+
         code = inputStream.readBits(read);
+
+
+
+
+
         //imprimo el codigo
-        System.out.println("code: "+code);
-        //imprimo lo q hay en el diccionario
-        System.out.println("leido " + dict[code]);  // salida
-        
+        System.out.println("code: "+Integer.toBinaryString(code)+ " " + code);
+        //METODO 2
+        //String outString;
+        //oldcode=dict.get(code);
+
+
+
+
+        //METODO 1
         oldcode=dict[code];
-        
-        while(bitsread<(offset-8)*8){
+
+        //METODO 3
+        //int oldcode;
+        //oldcode=code;
+        //char caracter=dict[code].charAt(0);
+        //String string;
+        while(bitsread<(offset-9)*8){
+
+            //METODO 3
+            /*
+            code = inputStream.readBits(read);
+            boolean isOnDict = false;
+            if(code<=iTable){
+              isOnDict=true;
+            }
+
+
+            if(isOnDict){
+                string=dict[code];
+            }else{
+                string=dict[oldcode];
+                string+=caracter;
+            }
+
+            System.out.println("out: "+string);
+            caracter = string.charAt(0);
+            dict[iTable++]= String.valueOf(oldcode)+caracter;
+            oldcode = code;
+             *
+             *
+             */
+
+
+             //METODO 1
             //este es el mismo codigo q hay en Test LZW
             code = inputStream.readBits(read);
-            System.out.println("code: "+code);
             System.out.println("leido " + dict[code]); // salida
             oldcode = oldcode.concat(dict[code]);
             dict[iTable++] =oldcode;
             oldcode=dict[code];
-            
-            
-            /* esto era un experimiento
+
+            //METODO 2
+            /*
+            System.out.println("dimencion: "+dict.size());
+
             code = inputStream.readBits(read);
+            System.out.println("code: "+Integer.toBinaryString(code)+ " " + code);
+
             boolean isOnDict = false;
             for(String d:dict){
-                if(d.equals(String.valueOf(code))){
+                if(d.equals((Character.toString((char)code)))){
                     isOnDict=true;
                     break;
                 }
             }
             if(isOnDict){
-                System.out.println("leido " + dict.get(StringFromTable(dict,String.valueOf(code))));  // salida
-                dict.add(dict.get(StringFromTable(dict,oldcode+String.valueOf((char)code))));
+                System.out.println("leido " + dict.get(code));  // salida
+                dict.add(dict.get(Integer.parseInt(oldcode)) + dict.get(code).charAt(0));
                 oldcode=String.valueOf(code);
             }else{
-                outString = dict.get(StringFromTable(dict,oldcode))+String.valueOf((char)code);
+                outString = dict.get(StringFromTable(dict,oldcode))+dict.get(StringFromTable(dict,oldcode)).charAt(0);
                 System.out.println("leido " + outString);
                 dict.add(outString);
                 oldcode=String.valueOf(code);
             }*/
             bitsread+=read;
         }
-        
-        
-        
-        
-        
-        
-        
-        
-            
-            
-        }
 
+
+
+
+
+
+
+
+
+
+        }
      }
     
     public int StringFromTable(List<String> dict,String search){

1f29436692732726f80aa1b02ed02389a01934c5
jhon.gonzalez28@gmail.com <jhon.gonzalez28@gmail.com@88bac944-d0fc-eefa-ac56-35fbec5b5244>
2011-04-11 05:24:27 +0000

    amplia pero no interpola(vista)

--- VisorImagen/src/View/ViewMain.java
+++ VisorImagen/src/View/ViewMain.java
@@ -201,6 +201,12 @@ public class ViewMain extends javax.swing.JFrame{
         jLabel3 = new javax.swing.JLabel();
         Color3 = new javax.swing.JPanel();
         Color4 = new javax.swing.JPanel();
+        Interpolar = new javax.swing.JFrame();
+        EjeX = new javax.swing.JTextField();
+        EjeY = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jButton3 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
         jTextField1 = new javax.swing.JTextField();
@@ -237,6 +243,7 @@ public class ViewMain extends javax.swing.JFrame{
         jRadioButtonMenuItem2 = new javax.swing.JRadioButtonMenuItem();
         jMenuItem4 = new javax.swing.JMenuItem();
         jMenuItem5 = new javax.swing.JMenuItem();
+        jMenuItem8 = new javax.swing.JMenuItem();
         jMenuItem7 = new javax.swing.JMenuItem();
         Comparar = new javax.swing.JMenu();
         jMenuItem3 = new javax.swing.JMenuItem();
@@ -452,6 +459,59 @@ public class ViewMain extends javax.swing.JFrame{
                     .addComponent(Color1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel3)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        Interpolar.setTitle("Interpolar");
+        Interpolar.setMinimumSize(new java.awt.Dimension(205, 120));
+        Interpolar.setResizable(false);
+
+        EjeY.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                EjeYActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setText("Eje X:");
+
+        jLabel5.setText("Eje Y:");
+
+        jButton3.setText("Interpolar");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout InterpolarLayout = new javax.swing.GroupLayout(Interpolar.getContentPane());
+        Interpolar.getContentPane().setLayout(InterpolarLayout);
+        InterpolarLayout.setHorizontalGroup(
+            InterpolarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(InterpolarLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(InterpolarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel5))
+                .addGap(20, 20, 20)
+                .addGroup(InterpolarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton3)
+                    .addComponent(EjeY, javax.swing.GroupLayout.DEFAULT_SIZE, 135, Short.MAX_VALUE)
+                    .addComponent(EjeX, javax.swing.GroupLayout.DEFAULT_SIZE, 135, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        InterpolarLayout.setVerticalGroup(
+            InterpolarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(InterpolarLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(InterpolarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(EjeX, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(InterpolarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(EjeY, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel5))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton3)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -705,6 +765,14 @@ public class ViewMain extends javax.swing.JFrame{
             }
         });
         Tranformadas.add(jMenuItem5);
+
+        jMenuItem8.setText("Interpolar");
+        jMenuItem8.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem8ActionPerformed(evt);
+            }
+        });
+        Tranformadas.add(jMenuItem8);
 
         jMenuItem7.setText("Gamma");
         jMenuItem7.addActionListener(new java.awt.event.ActionListener() {
@@ -1130,6 +1198,25 @@ public class ViewMain extends javax.swing.JFrame{
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField4ActionPerformed
 
+    private void EjeYActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_EjeYActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_EjeYActionPerformed
+
+    private void jMenuItem8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem8ActionPerformed
+    Interpolar.setVisible(true);
+    EjeX.setText(String.valueOf(painterTexture.getLastElement().getDataOut().getImageData().getWidth()));
+    EjeY.setText(String.valueOf(painterTexture.getLastElement().getDataOut().getImageData().getHeight()));
+    }//GEN-LAST:event_jMenuItem8ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        int x = Integer.parseInt(EjeX.getText().trim());
+        int y = Integer.parseInt(EjeY.getText().trim());
+        InterpolateFilter gamma = new InterpolateFilter("Filtro Interpolar",x,y);
+        gamma.setLastElement(painterTexture.getLastElement());
+        painterTexture.setLastElement(gamma);
+        painterTexture.Update();
+    }//GEN-LAST:event_jButton3ActionPerformed
+
     /**
     * @param args the command line arguments
     */
@@ -1157,12 +1244,15 @@ public class ViewMain extends javax.swing.JFrame{
     private javax.swing.JPanel Color6;
     private javax.swing.JMenu Comparar;
     private javax.swing.JMenu Compresor;
+    private javax.swing.JTextField EjeX;
+    private javax.swing.JTextField EjeY;
     private javax.swing.ButtonGroup FiltorsModelos;
     private javax.swing.JMenu Filtros;
     private javax.swing.ButtonGroup FiltrosBase;
     private javax.swing.JLabel Grados;
     private javax.swing.JFrame HistogramaColor;
     private javax.swing.JFrame HistogramaColor1;
+    private javax.swing.JFrame Interpolar;
     private javax.swing.JMenu ModelosColor;
     private javax.swing.JRadioButtonMenuItem RadioFilterB;
     private javax.swing.JRadioButtonMenuItem RadioFilterCMY;
@@ -1181,10 +1271,13 @@ public class ViewMain extends javax.swing.JFrame{
     private javax.swing.JMenu Tranformadas;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JComboBox jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
@@ -1196,6 +1289,7 @@ public class ViewMain extends javax.swing.JFrame{
     private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JMenuItem jMenuItem6;
     private javax.swing.JMenuItem jMenuItem7;
+    private javax.swing.JMenuItem jMenuItem8;
     private javax.swing.JRadioButtonMenuItem jRadioButtonMenuItem1;
     private javax.swing.JRadioButtonMenuItem jRadioButtonMenuItem2;
     private javax.swing.JScrollPane jScrollPane1;
